5d4972023ae9fed1ce5b239af67bb156585c435c
Amr Maher <106030258+01022270213Amr@users.noreply.github.com>
2026-08-17 01:25:06 +0300

    Implement print functionality in Main method
    Added functionality to print formatted numbers and current date.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Print
+            //string m = "Hello World!";
+
+
+            //Console.WriteLine(Alfa(m));
+            //Console.WriteLine(40 * 30);
+
+            Console.WriteLine(Alfa(Convert.ToString(320.00.ToString("N2"))));
+            Console.WriteLine(Alfa(Convert.ToString(320.00)));
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+
+            //Console.WriteLine(Alfa(Convert.ToString(320.54)));
+
+            //Console.WriteLine(Alfa(Convert.ToString(320.00.ToString("C"))));
+
+        }
+
+        static string Alfa(string s) 
+        {
+            return s;
         }
     }
 }
