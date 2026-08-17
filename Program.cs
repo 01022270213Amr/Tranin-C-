4a6e7156d167cedda6a861cca1fb62036870c01a
Amr Maher <106030258+01022270213Amr@users.noreply.github.com>
2026-08-17 17:29:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
@@ -12,7 +12,6 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Print
-            
             string m = "Hello World!";
             Console.WriteLine(Alfa(m));
             Console.WriteLine(40 * 30);
@@ -25,7 +24,6 @@ namespace ConsoleApp1
             //C# Type Casting
             //Explicit Casting
             //Explicit casting must be done manually by placing the type in parentheses in front of the value:
-            
             double x = 4.00;
             int y = (int)x;
 
@@ -33,9 +31,7 @@ namespace ConsoleApp1
             //It is also possible to convert data types explicitly by using built-in methods, such as Convert.ToBoolean,
             //Convert.ToDouble, Convert.ToString, Convert.ToInt32(int) and Convert.ToInt64(long):
             //Case one
-
             int myInt = 10;
-
             double myDouble = 5.25;
             bool myBool = true;
 
@@ -43,9 +39,7 @@ namespace ConsoleApp1
             Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
             Console.WriteLine(Convert.ToInt32(myDouble));  // convert double to int
             Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
-            
             //Case Two
-            
             string u = "30";
             string d = "60";
 
@@ -58,7 +52,6 @@ namespace ConsoleApp1
             Console.WriteLine(name);
 
             //Short Hand If...Else(Ternary Operator)
-            
             Console.Write("Entr your age : ");
             int age = int.Parse(Console.ReadLine());
             string result = (age > 16) ? "Done" : "Feald";
